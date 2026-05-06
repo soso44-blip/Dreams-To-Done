0671d48e517dd1e7e2bbb20bebc4105679b2365a
soso44-blip <welcome.greenbox@gmail.com>
2026-05-06 15:38:29 +0800

    Add files via upload

---
+++ sw-student-light.js
@@ -0,0 +1,24 @@
+const CACHE = 'adhd-student-light-v2';
+const ASSETS = [
+  './student-light.html',
+  'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap',
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(ASSETS.map(u => c.add(u).catch(() => {})))));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => {
+    if (cached) return cached;
+    return fetch(e.request).then(r => {
+      if (!r || r.status !== 200 || r.type === 'opaque') return r;
+      const cl = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cl));
+      return r;
+    }).catch(() => caches.match('./student-light.html'));
+  }));
+});
